94fdd6d8e0dbc2273a13220e559b55b95454d28c
GalvisDev <galvisdeveloper@gmail.com>
2024-06-04 22:15:04 -0500

    Unit testing finalizado

--- tests/app.test.ts
+++ tests/app.test.ts
@@ -1,5 +1,16 @@
 
+// process.argv = ['node', 'app', '--base', '2'];
+// import '../src/app'
+
+import { ServerApp } from "../src/presentation/server-app";
+
 describe('App', () => {
-
-
+    test('should initialized app', async () => {
+        const serverRunMock = jest.fn();
+        ServerApp.run = serverRunMock;
+        process.argv = ['node', 'app', '--base', '2'];
+        await import('../src/app');
+        expect(serverRunMock).toHaveBeenCalledTimes(1);
+        expect(serverRunMock).toHaveBeenCalledWith({ base: 2, limit: 10, show: false, path: 'outputs/', name: 'table' });
+    })
 });
